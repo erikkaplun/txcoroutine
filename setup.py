836f619249c7758d5428665932b0741680a94488
Erik Allik <erik.allik@skype.net>
2012-08-17 20:41:46 +0300

    Bumped version to 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README') as file:
 setup(
     name="twisted-coroutine",
     long_description=long_description,
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
 
     install_requires=[
