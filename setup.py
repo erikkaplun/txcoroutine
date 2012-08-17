ee8e94665ec55a15471ea2d40666de8e6bccec01
Erik Allik <erik.allik@skype.net>
2012-08-17 20:48:36 +0300

    Bumped version to 1.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
 setup(
     name="twisted-coroutine",
     long_description=long_description,
-    version="1.0.5",
+    version="1.0.6",
     packages=find_packages(),
 
     install_requires=[
